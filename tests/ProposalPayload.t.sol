3953d49b23fcc175407f9f32e7ca25a6cd0fb6ef
Kogaroshi <25688223+Kogaroshi@users.noreply.github.com>
2026-04-28 15:33:54 +0100

    feat : proposal payload

---
+++ tests/ProposalPayload.t.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import 'forge-std/Test.sol';
+
+import {IERC20} from 'openzeppelin-contracts/contracts/token/ERC20/IERC20.sol';
+import {AaveV2EthereumAssets} from 'aave-address-book/AaveV2Ethereum.sol';
+import {AaveV3Ethereum} from 'aave-address-book/AaveV3Ethereum.sol';
+import {GovV3Helpers} from 'aave-helpers/GovV3Helpers.sol';
+
+import {IInitializableAdminUpgradeabilityProxy} from 'src/interfaces/IInitializableAdminUpgradeabilityProxy.sol';
+import {LendToAaveMigrator} from 'src/LendToAaveMigrator.sol';
+import {ProposalPayload} from 'src/proposals/ProposalPayload.sol';
+
+contract ProposalPayloadTest is Test {
+  IERC20 public constant AAVE = IERC20(AaveV2EthereumAssets.AAVE_UNDERLYING);
+  IERC20 public constant LEND = IERC20(0x80fB784B7eD66730e8b1DBd9820aFD29931aab03);
+  uint256 public constant LEND_AAVE_RATIO = 100;
+  address public constant COLLECTOR = address(AaveV3Ethereum.COLLECTOR);
+
+  address payable public migratorProxyAddress = payable(0x317625234562B1526Ea2FaC4030Ea499C5291de4);
+
+  LendToAaveMigrator public migratorImpl;
+  IInitializableAdminUpgradeabilityProxy public migratorProxy;
+  LendToAaveMigrator public migrator;
+  ProposalPayload public payload;
+
+  function setUp() public {
+    vm.createSelectFork(vm.rpcUrl('mainnet'));
+
+    migratorImpl = new LendToAaveMigrator(AAVE, LEND, LEND_AAVE_RATIO, COLLECTOR);
+    migratorProxy = IInitializableAdminUpgradeabilityProxy(migratorProxyAddress);
+    migrator = LendToAaveMigrator(migratorProxyAddress);
+
+    payload = new ProposalPayload(address(migratorImpl));
+  }
+
+  function test_execute() public {
+    assertEq(migratorProxy.REVISION(), 2);
+
+    uint256 preMigratorAaveBalance = AAVE.balanceOf(address(migrator));
+    uint256 preCollectorAaveBalance = AAVE.balanceOf(COLLECTOR);
+
+    vm.expectEmit(address(migrator));
+    emit LendToAaveMigrator.AaveTokensRescued(address(migrator), COLLECTOR, preMigratorAaveBalance);
+    GovV3Helpers.executePayload(vm, address(payload));
+
+    assertEq(migratorProxy.REVISION(), 3);
+    assertEq(AAVE.balanceOf(address(migrator)), 0);
+    assertEq(AAVE.balanceOf(COLLECTOR), preCollectorAaveBalance + preMigratorAaveBalance);
+    assertTrue(migrator.migrationStarted());
+    assertTrue(migrator.migrationEnded());
+
+    vm.expectRevert(LendToAaveMigrator.MigrationClosed.selector);
+    migrator.migrateFromLEND(1);
+  }
+
+  function test_execute_immutables() public view {
+    assertEq(payload.LEND_TO_AAVE_MIGRATOR_IMPL(), address(migratorImpl));
+    assertEq(payload.LEND_TO_AAVE_MIGRATOR_PROXY(), migratorProxyAddress);
+  }
+}
